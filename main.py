56fa4f059cf7f55ba672535d4bf760c42bad3c27
Hamza ince <hamzaince10@gmail.com>
2023-09-10 00:13:42 +0300

    10.09.2023

--- main.py
+++ main.py
@@ -8,34 +8,33 @@ metin = """
 Bu, Ali ve Ayşe'nin hikayesinin sonu, ama arkadaşlık, macera ve keşiflerin hikayesi hiçbir zaman bitmezdi. Ve bu hikayeler, yeni nesillere ilham verir, hayatın güzelliklerini ve değerini hatırlatır ve arkadaşlığın gücünü anlatan bir miras olarak yaşamaya devam ederdi.
 ]"""
 
-# Metni bir ses dosyasına dönüştür
 tts = gTTS(text=metin, lang='tr')
 
-# Ses dosyasını kaydetme işlemi
+
 ses_dosyasi_adi = "sesdosyasi.mp3"
 
-# İşlem başladı mesajı
+
 print("Ses dosyası oluşturuluyor... ", end='', flush=True)
 
-# Başlangıç zamanını al
+
 baslangic_zamani = time.time()
 
-# Ses dosyasını kaydetmek için başla
+
 tts.save(ses_dosyasi_adi)
 
-# Bitiş zamanını al
+
 bitis_zamani = time.time()
 
-# İşlem süresini hesapla
+
 isleme_suresi = bitis_zamani - baslangic_zamani
 
-# İşlem tamamlandı mesajını yazdır
+
 print(f"\nSes dosyası başarıyla oluşturuldu: {ses_dosyasi_adi}")
 
-# İşlem süresini ve ilerlemeyi göster
+
 print(f"İşlem süresi: {isleme_suresi:.2f} saniye")
 
-# İlerleme durumunu ve işlem süresini göster
+
 for i in range(101):
     sys.stdout.write(f"\rİlerleme: %{i} İşlem süresi: {isleme_suresi:.2f} saniye")
     sys.stdout.flush()
